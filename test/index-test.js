7aec2ee40835ec48aeadd7ad3436e78d72fb82c9
Danielle Deavens <danielledeavens@gmail.com>
2018-07-02 18:40:33 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,8 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)'), function shout(string) {return string.toUpperCase() ;}
+  it('receives one argument and returns it in all caps', function() 
     expect(shout('hello')).toEqual('HELLO')
-  })
+  )})
 })
 
 describe('whisper(string)', function() {
